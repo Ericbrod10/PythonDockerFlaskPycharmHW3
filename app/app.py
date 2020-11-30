67e6cb24d676dbc6aac4134bc5ec51f3cea4062b
Eric <56181235+Ericbrod10@users.noreply.github.com>
2020-11-29 23:49:49 -0500

    Fixes

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Zillow Project'}
+    user = {'username': 'Eric Project'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM zillow')
     result = cursor.fetchall()
